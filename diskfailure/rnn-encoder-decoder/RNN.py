c36dfdd2e1c4c6d76f39c87c626c24c932f47064
unknown <wujian1996819@qq.com.com>
2020-09-07 18:53:18 +0800

    2020/9/7

--- diskfailure/rnn-encoder-decoder/RNN.py
+++ diskfailure/rnn-encoder-decoder/RNN.py
@@ -32,7 +32,7 @@ class RNN(object):
             _, softmax, hidden_state = self.decoder.forward(target[i], hidden_state)
 
             outputs.append(np.argmax(softmax.data.numpy(), 1)[:, np.newaxis])
-            total_loss += self.loss(softmax, target[i + 1].squeeze(1))
+            total_loss += self.loss(softmax, target[i].squeeze(1))
 
         total_loss /= len(outputs)
         total_loss.backward()
--- diskfailure/rnn-encoder-decoder/main.py
+++ diskfailure/rnn-encoder-decoder/main.py
@@ -1,4 +1,39 @@
 from RNN import *
+from MyData import 
+
+import collections
+import math
+import os
+import random
+import sys
+import tarfile
+import time
+import json
+from collections import namedtuple
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torchtext
+import torchtext.vocab as Vocab
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
+from torch.optim import lr_scheduler
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_data = pd.read_csv('F:/data/train_2018Q1_model_2.csv')
+
+num_epochs = 10
+batch_size = 10
+numstep = 7
+length = 6
 
 def main():
-    data = 
+    data = MyData(train_data, numstep, length)
+    sampler = mySample(data)
+    train_iter = DataLoader(data, batch_size = 128, drop_last=True, sampler=sampler)
+
+    losses = []
+    for epoch in range(num_epochs)
